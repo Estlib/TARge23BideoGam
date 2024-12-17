d4700e8e4b3159bb2d308bfd639ec9a31f6acda2
Estlib <75094683+Estlib@users.noreply.github.com>
2024-12-17 10:27:09 +0200

    Delivers #1 #2 #3 https://trello.com/c/qqFZzEPD/22-as-an-app-developer-i-want-to-see-details-of-a-specific-comment

--- BideoGam-api/controllers/CommentsController.js
+++ BideoGam-api/controllers/CommentsController.js
@@ -12,3 +12,23 @@ exports.getAll = async (req,res) => {
         )
     )
 }
+
+exports.getById = async (req, res) => {
+    const comment = await getComment(req, res);
+    if (!comment) { return };
+    return res.send(comment);
+}
+
+const getComment = async (req, res) => {
+    const idNumber = parseInt(req.params.CommentID);
+    if (isNaN(idNumber)) {
+        res.status(400).send({error: `Invalid comment ID ${req.params.CommentID}`});
+        return null;
+    }
+    const comment = await db.comments.findByPk(idNumber);
+    if (!comment) {
+        res.status(404).send({error: `Comment with ID ${idNumber} not found`});
+        return null;
+    }
+    return comment;
+}
