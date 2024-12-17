29cee28c02549399cfb65633ad13b617c51fcb5e
Estlib <75094683+Estlib@users.noreply.github.com>
2024-12-17 11:43:37 +0200

    trying to create association

---
+++ BideoGam-api/models/GameComment.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize,DataTypes,Game,Comment) => {
+    const GameComment = sequelize.define('GameComment', {
+        GameCommentID: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        GameID: {
+            type: DataTypes.FOREIGNKEY,
+            // autoIncrement: false,
+            // primaryKey: false,
+            references:{
+                model: Game,
+                key: "GameID"
+            }
+        },
+        CommentID: {
+            type: DataTypes.FOREIGNKEY,
+            // autoIncrement: false,
+            // primaryKey: false,
+            references:{
+                model: Comment,
+                key: "CommentID"
+            }
+        },
+    });
+    Game.BelongsToMany(Comment, {through: GameComment})
+    Comment.BelongsToMany(Game, {through: GameComment})
+    Game.HasMany(GameComment)
+    GameComment.BelongsTo(Game)
+    Comment.HasMany(GameComment)
+    GameComment.BelongsTo(Comment);
+
+
+    console.log(GameComment === sequelize.models.GameComment);
+    return GameComment;
+}
