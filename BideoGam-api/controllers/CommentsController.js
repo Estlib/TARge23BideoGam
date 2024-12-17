d6e33db6f1fbe6749ddb22c6ad283aeb896cd208
Estlib <75094683+Estlib@users.noreply.github.com>
2024-12-17 10:09:36 +0200

    Delivers https://trello.com/c/OTtGsn1o/20-as-an-app-developer-i-want-to-be-able-to-see-all-comments-in-my-app

--- BideoGam-api/controllers/CommentsController.js
+++ BideoGam-api/controllers/CommentsController.js
@@ -1,2 +1,14 @@
 const {db} = require("../db");
 const Utils = require("./utils");
+
+exports.getAll = async (req,res) => {
+    const allcomments = await db.comments.findAll();
+    console.log(allcomments)
+    res
+    .send(allcomments
+        .map((
+            {CommentID: CommentText}) =>
+            {return {CommentID: CommentText}}
+        )
+    )
+}
